e4e0c3464332e0f3f95aac7e4ff606b5445d0fd3
Arman Mia <mdarman365506@gmail.com>
2025-10-25 00:02:40 +0600

    Fix: added fallback image handling for invalid or missing project/blog thumbnails

--- src/app/(dashboard)/dashboard/projects/page.tsx
+++ src/app/(dashboard)/dashboard/projects/page.tsx
@@ -4,7 +4,7 @@ import Projects from "@/components/modules/Home/Projects";
 const ProjectsPage = () => {
     return (
         <div>
-            <h1>this is porject page</h1>
+           
             <Projects isDashboard={true} />
         </div>
     );
